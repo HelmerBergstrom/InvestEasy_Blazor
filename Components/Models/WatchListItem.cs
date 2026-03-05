57289ca5e5f411f0aa830c9000bb99319582fbe8
Helmer Bergström <hebe2400@student.miun.se>
2026-03-05 16:13:44 +0100

    Skapar sidan WatchList.razor. Gör så denna sida kräver inloggning. Skapar modell för WatchListItem, som användaren ska kunna lägga in i watchlist.

--- Components/Models/WatchListItem.cs
+++ Components/Models/WatchListItem.cs
@@ -1,6 +1,19 @@
 namespace InvestEasy.Models;
 
-public class WatchListItemModel
+public class WatchListItem
 {
     public int Id { get; set; }
+
+    public string UserId { get; set; } = "";
+
+    // Finnhub symbol for search of quote
+    public string Symbol { get; set; } = "";
+
+    // Name of the stock
+    public string Name { get; set; } = "";
+
+    // ex: Common stock, ETF etc.
+    public string? Type { get; set; }
+
+    public DateTime CreatedUtc { get; set; } = DateTime.UtcNow;
 }
